cfcef83af32fdb73ffe0995c21c5b28831ede794
ChandupaSH <chandupash107@gmail.com>
2023-12-13 16:54:11 +0530

    first commit

---
+++ src/components/SideMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SideMenu = () => {
+  return (
+    <div>
+      <span>SideMenu</span>
+    </div>
+  )
+}
+
+export default SideMenu
---
+++ src/components/SlideShow.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SlideShow = () => {
+  return (
+    <div>
+      <span>slideshow</span>
+    </div>
+  )
+}
+
+export default SlideShow
